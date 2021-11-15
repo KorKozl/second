b6a41e08983e6a1796980684d4bca84332854d28
Kordian Kozłowski <KorKoz@Pracownia.com>
2021-11-15 15:58:03 +0100

    'gog'

---
+++ gog.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GOG</title>
+    <link rel="stylesheet" href="gog.css">
+
+</head>
+<body>
+
+<div class="up">
+
+<div class="logo">
+
+<a><img src="logo.png" alt="steam" width="200" height="60"></a>
+</div>
+<div class="menu-content">
+<a href="url">Biblioteka</a>   
+  
+</div> 
+
+
+</div>
+
+
+<div class="middle">
+
+</div>
+
+
+
+    
+</body>
+</html>
